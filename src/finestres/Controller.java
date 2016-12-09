cf2a717804de4c67e08eb1bae9a84390991e4019
rcasamajo <rcasamajo@iespoblenou.org>
2016-12-09 19:41:53 +0100

    Initial commit

---
+++ src/finestres/Controller.java
@@ -0,0 +1,11 @@
+package finestres;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+
+public class Controller {
+    public MenuItem miNewWin;
+
+    public void openWindow(ActionEvent actionEvent) {
+    }
+}
---
+++ src/finestres/SecController.java
@@ -0,0 +1,7 @@
+package finestres;
+
+/**
+ * Created by rcasamajo on 09/12/16.
+ */
+public class SecController {
+}
